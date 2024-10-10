afa15057f2ad4bef281c7db2b7ac0cc8e0c3567e
MyeongJun Kim <audwns990625@gmail.com>
2024-10-10 09:28:29 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<stdio.h>
+#include <vector>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+	cout << a + b;
+    return 0;
+}
