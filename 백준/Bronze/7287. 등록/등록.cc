b4a0a85240eb46a7d75b8bcb3815930f428350b8
MyeongJun Kim <audwns990625@gmail.com>
2024-10-10 09:40:31 +0900

    [Bronze V] Title: 등록, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.cc
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include<stdio.h>
+#include <vector>
+#include <stack>
+#include <string>
+#include <unordered_set>
+#include <algorithm>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    cout << 137<< endl << "audwns2862";
+    return 0;
+}
