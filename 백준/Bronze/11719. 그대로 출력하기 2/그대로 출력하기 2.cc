1fe3269d4923a5803afa6640f477e5a25445bb23
MyeongJun Kim <audwns990625@gmail.com>
2024-10-16 12:04:44 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+
+
+int main()
+{
+	string input;
+	while (getline(cin, input))
+		cout << input<<endl;
+
+	return 0;
+}
