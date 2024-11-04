6ce16e4fa03155de4305cf8b49190b5f997ed37d
MyeongJun Kim <audwns990625@gmail.com>
2024-11-05 08:47:18 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> solution(vector<int> num_list) {
+    vector<int> answer;
+    int len = num_list.size();
+    sort(num_list.begin(), num_list.end());
+    for(int i =0;i<5;i++)
+    {
+        answer.push_back(num_list[i]);
+    }
+    return answer;
+}
